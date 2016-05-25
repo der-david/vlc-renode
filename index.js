1908f35ac400583c822589aaff044ccbe068294b
der-david <github@davdev.de>
2016-05-25 16:09:42 +0200

    cleanup

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ module.exports = class VLCRenode {
         username: this._username,
         password: this._password
       },
-      headers: {
-          'Content-Type': 'application/json'
-      },
       json: true
     };
   }
